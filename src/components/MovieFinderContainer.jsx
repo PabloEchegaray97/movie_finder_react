fd162afa4add7554ac30719186aa29e7bc363f30
pae <pablo.echegaray97@gmail.com>
2023-04-25 13:07:39 -0300

    pagination

--- src/components/MovieFinderContainer.jsx
+++ src/components/MovieFinderContainer.jsx
@@ -4,23 +4,34 @@ import Movie from './Movie'
 const MovieFinderContainer = () => {
     const [title, setTitle] = useState('');
     const [movies, setMovies] = useState([]);
+    const [count, setCount] = useState(0);
+    const [page, setPage] = useState(1);
     const titleOnChangeHandler = (event) => {
         setTitle(event.target.value);
     };
-    
+    const nextPageClickHandler=() => {
+        setPage(page+1)
+    }
+    const prevPageClickHandler=() => {
+        if (page>1) {
+            setPage(page-1)
+        }
+    }
     useEffect(() => {
-        fetch(`https://www.omdbapi.com/?apikey=a8f52dc1&s=${title}`)
+        fetch(`https://www.omdbapi.com/?apikey=a8f52dc1&s=${title}&page=${page}`)
             .then((res) => res.json())
             .then((resmovies) => {
                 if (resmovies.Search) {
                     setMovies(resmovies.Search);
+                    setCount(resmovies.totalResults)
                 }
             });
-    }, [title]);
+    }, [title,page]);
     
     return (
         <div>
             <div className="form-container">
+            <span>Peliculas encontradas: {count}</span>
             <label htmlFor="title">Buscar título</label>
             <input id="title" onChange={titleOnChangeHandler} value={title} />
             </div>
@@ -29,6 +40,8 @@ const MovieFinderContainer = () => {
                 <Movie movie={movie} key={movie.imdbID} />
             ))}
             </div>
+            <button onClick={prevPageClickHandler}>prev</button>
+            <button onClick={nextPageClickHandler}>next</button>
         </div>
     )
 }
